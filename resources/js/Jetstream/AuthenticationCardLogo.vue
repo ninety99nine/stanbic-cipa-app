90e4928ae928506eb22738ebf661d692919c1a55
brandontabona <brandontabona@gmail.com>
2021-04-13 17:15:00 +0200

    Fresh Installation - Modified Logo & Button Components

---
+++ resources/js/Jetstream/AuthenticationCardLogo.vue
@@ -0,0 +1,15 @@
+<template>
+    <inertia-link :href="'/'">
+        <jet-application-logo :scale="1.5" />
+    </inertia-link>
+</template>
+
+<script>
+    import JetApplicationLogo from '@/Jetstream/ApplicationLogo'
+
+    export default {
+        components: {
+            JetApplicationLogo,
+        },
+    }
+</script>
