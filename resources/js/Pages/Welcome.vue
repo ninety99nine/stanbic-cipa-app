fe84bed19b7e528a9c734881adbbef686d58c89a
brandontabona <brandontabona@gmail.com>
2021-06-18 13:48:11 +0200

    Removed resgister link from welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -9,8 +9,6 @@
 
                 <template v-else>
                     <inertia-link :href="route('login')" class="text-sm text-gray-700 underline">Log in</inertia-link>
-
-                    <inertia-link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 underline">Register</inertia-link>
                 </template>
             </div>
         </div>
